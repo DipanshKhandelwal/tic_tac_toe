53b750ea4d4634f3dfa224500f718d6a114cacf7
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2019-04-08 05:02:07 +0530

    Add minimal tic tac toe

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,151 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return new MaterialApp(
+      home: new Game(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Game extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GameState createState() => new _GameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GameState extends State<Game> {
+  List<Button> btns;
+  var p1,p2,active;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    btns = init();
+  }
+
+  List<Button> init() {
+    p1 = new List();
+    p2 = new List();
+    active = 1;
+
+    var buttons = new List<Button>.generate(9, (int index) => Button(id: index));
+    return buttons;
+  }
+
+  void playGame(Button gb) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      if (active == 1) {
+        gb.text = "X";
+        gb.bg = Colors.red;
+        active = 2;
+        p1.add(gb.id);
+      } else {
+        gb.text = "0";
+        gb.bg = Colors.blue;
+        active = 1;
+        p2.add(gb.id);
+      }
+      gb.enabled = false;
+      int winner = checkWinner();
+      if (winner == -1) {
+        if (btns.every((p) => p.text != "")) {
+          dialog("Game Tie !!");
+        }
+      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+  bool chk(p,a,b,c) {
+    return p.contains(a) && p.contains(b) && p.contains(c);
+  }
+
+  int checkWinner() {
+    var winner = -1;
+    if(chk(p1,0,1,2) || chk(p1,3,4,5) || chk(p1,6,7,8) ||
+        chk(p1,0,3,6) || chk(p1,1,4,7) || chk(p1,2,5,8) ||
+        chk(p1,0,4,8) || chk(p1,2,4,6)) { winner = 1; }
+
+    if(chk(p2,0,1,2) || chk(p2,3,4,5) || chk(p2,6,7,8) ||
+        chk(p2,0,3,6) || chk(p2,1,4,7) || chk(p2,2,5,8) ||
+        chk(p2,0,4,8) || chk(p2,2,4,6)) { winner = 2; }
+
+    if (winner != -1) { dialog("Player $winner Won !!"); }
+    return winner;
+  }
+
+  void reset() {
+    if (Navigator.canPop(context)) Navigator.pop(context);
+    setState(() { btns = init(); });
+  }
+
+  void dialog(text) {
+    showDialog( context: context,
+        builder: (_) => new AlertDialog(
+          title: new Text(text),
+          actions: <Widget>[
+            new FlatButton(
+              onPressed: reset,
+              color: Colors.white,
+              child: new Text("Reset"),
+            )],
+        )
     );
   }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Center(
+            child: Text("Tic Tac Toe"),
+          ),
+        ),
+        body: new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            new Expanded(
+              child: new GridView.builder(
+                padding: const EdgeInsets.all(10.0),
+                gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    childAspectRatio: 1.0,
+                    crossAxisSpacing: 9.0,
+                    mainAxisSpacing: 9.0),
+                itemCount: btns.length,
+                itemBuilder: (context, i) => new SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: new RaisedButton(
+                    padding: const EdgeInsets.all(8.0),
+                    onPressed: btns[i].enabled
+                        ? () => playGame(btns[i])
+                        : null,
+                    child: new Text( btns[i].text,
+                        style: new TextStyle(
+                            color: Colors.white, fontSize: 20.0) ),
+                    color: btns[i].bg,
+                    disabledColor: btns[i].bg,
+                  ),
+                ),
+              ),
+            ),
+            new RaisedButton(
+              child: new Text( "Reset",
+                  style: new TextStyle(color: Colors.white, fontSize: 20.0) ),
+              color: Colors.red,
+              padding: const EdgeInsets.all(20.0),
+              onPressed: reset,
+            )
+          ],
+        ));
+  }
+}
+
+class Button {
+  final id; String text; Color bg; bool enabled;
+  Button({this.id, this.text = "", this.bg = Colors.grey, this.enabled = true});
 }
