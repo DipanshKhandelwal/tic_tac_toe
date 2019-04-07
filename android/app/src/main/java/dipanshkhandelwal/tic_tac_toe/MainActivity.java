87ffd874178c492b9f7810f95d3c1a2675ecb645
DipanshKhandelwal <deepanshrocks438@gmail.com>
2019-04-08 04:59:03 +0530

    Initialized repo

---
+++ android/app/src/main/java/dipanshkhandelwal/tic_tac_toe/MainActivity.java
@@ -0,0 +1,13 @@
+package dipanshkhandelwal.tic_tac_toe;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
